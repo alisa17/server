3273e7dce6476a41a46d9c1d489a5b11660d5c72
kfrn <knfrances@gmail.com>
2016-12-08 18:52:27 +1300

    get users test passing with object in memory

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+var userDb = [
+  { "username": "kfrn",
+    "id": 1,
+    "password": "admin",
+    "shotsRemaining": 3,
+    "email": "knfrances@gmail.com"
+   },
+  { "username": "symeshjb",
+    "id": 2,
+    "password": "memes",
+    "shotsRemaining": 3,
+    "email": "symeshjb@gmail.com"
+  }
+]
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.status(200)
-  res.json({
-        "users": [
-            { "username": "kfrn",
-              "user_id": 1 },
-            { "username": "symeshjb",
-              "user_id": 4 } ]
+  var obj = { "users": []}
+  obj.users = userDb.map((user) => {
+    return {"username": user.username, "user_id": user.id}
   })
+  res.json(obj)
 })
 
 router.post('/signup', (req, res, next) => {
