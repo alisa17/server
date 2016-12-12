a4e7d77adb00a664ec2f67919a6db7d8e91b0ca2
kfrn <knfrances@gmail.com>
2016-12-12 17:49:01 +1300

    create comments table and entries function to increment the commentCount (new column) by 1 for when a comment is added

---
+++ migrations/20161212174521_comments.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  knex.schema.createTableIfNotExists('comments', (table) => {
+    table.increments('comment_id')
+    table.integer('entry_id')
+    table.integer('user_id')
+    table.timestamp('comment_created_at').defaultTo(knex.fn.now())
+  })
+};
+
+exports.down = function(knex, Promise) {
+
+};
