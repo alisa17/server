a823925527dcfe3b19ac15b8889935c960f081b7
harrison-symes <symeshjb@gmail.com>
2016-12-13 04:58:04 +1300

    add knex functions, route skeletons, and documentation for follows functionality on server side

---
+++ migrations/20161213030528_follows.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTableIfNotExists('follows', (table) => {
+    table.increments('id')
+    table.integer('followed_user_id')
+    table.integer('following_user_id')
+
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('follows')
+};
