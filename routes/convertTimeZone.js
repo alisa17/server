ba164757d4ddff38a980f8bbedff94ecedb3d804
kfrn <knfrances@gmail.com>
2016-12-14 15:34:20 +1300

    Finally formatting dates correctly with timezone offset:

--- routes/convertTimeZone.js
+++ routes/convertTimeZone.js
@@ -1,14 +1,21 @@
-const moment = require('moment-timezone')
+const moment = require('moment')
+const momentTZ = require('moment-timezone')
 
 module.exports = convertTimeZone
 
  function convertTimeZone(timeinUTC) {
-   const localTZ = 'Pacific/Auckland'
-   var inLocalTime = moment.tz(timeinUTC, localTZ).format()
-  //  console.log({inLocalTime});
-   return inLocalTime
+
+   var setUTC = moment.utc(timeinUTC).format()
+   var localTime = moment(setUTC).local().format()
+   var formatted = moment(localTime).format('HH:mma, Do MMM')
+
+   console.log("UTC date is: ", setUTC)
+   console.log("localDate is: ", localTime)
+   console.log("now formatted: ", formatted)
+
+   return formatted
  }
 
 // Testing
-// var inUTC = '2016-12-13 21:46:45'
-// convertTimeZone(inUTC)
+var inUTC = '2016-12-13 21:46:45'
+convertTimeZone(inUTC)
